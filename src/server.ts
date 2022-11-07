c75b893210fd097bc92b8714d8e323d69ab90325
Rodrigo Bussolo <rbussolo91@gmail.com>
2022-11-07 08:03:20 -0400

    Adicionado cors ao projeto e alterado alguns caminhos dos serviços.

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,5 @@
 import express from "express";
+import cors from "cors";
 
 import { AppDataSource, EmalDataSource } from "./data-source";
 import { router } from "./routes";
@@ -7,8 +8,9 @@ AppDataSource.initialize();
 EmalDataSource.initialize();
 
 const app = express();
-const port = 3000;
+const port = 3333;
 
+app.use(cors());
 app.use(express.json());
 app.use(router);
 
