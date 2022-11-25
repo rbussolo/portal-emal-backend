cba392df4c256e60ea27aef5f8fe75655ad26840
Rodrigo Bussolo <rbussolo91@gmail.com>
2022-11-24 23:24:33 -0400

    Removido verificação de acesso, depois será tratado isso...

--- src/modules/users/useCases/updateUser/UpdateUserController.ts
+++ src/modules/users/useCases/updateUser/UpdateUserController.ts
@@ -9,11 +9,6 @@ export class UpdateUserController {
     const id: number = +request.params.id;
     const { name, cpf_cnpj, cellphone } = request.body;
 
-    // Check if it user can edit this user;
-    if(request.user.id != id && request.user.type == UserType.Client){
-      return response.status(400).json({ message: "Você não tem permissão para fazer esta operação!" });
-    }
-
     const service = new UpdateUserService();
     const result = await service.execute({ id, name, cpf_cnpj, cellphone });
 
