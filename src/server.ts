fee7840b0d8efbb0901c3113f10187a45d71b422
Rodrigo Bussolo <rbussolo91@gmail.com>
2022-11-27 10:58:54 -0400

    Organizado questão das rotas. Adicionado um tratamento de erro.

--- src/server.ts
+++ src/server.ts
@@ -9,6 +9,7 @@ import cors from "cors";
 import { AppDataSource } from "./data-source";
 import { EmalDataSource } from './data-source-emal';
 import { router } from "./routes";
+import handleError from './middlewares/ErrorHandler';
 
 AppDataSource.initialize();
 
@@ -22,5 +23,6 @@ const port = 3333;
 app.use(cors());
 app.use(express.json());
 app.use(router);
+app.use(handleError);
 
 app.listen(port, () => console.log("Servidor esta rodando na porta: " + port));
